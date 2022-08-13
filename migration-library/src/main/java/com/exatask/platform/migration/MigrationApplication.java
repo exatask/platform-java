78d91e7dc65058b79a699b673f430f0ebb907d76
Rohit Aggarwal <er.rohitaggarwal1989@gmail.com>
2022-08-14 01:55:05 +0530

    Adding ContextRefreshedEvent to migration, and stop web server in sdk

--- migration-library/src/main/java/com/exatask/platform/migration/MigrationApplication.java
+++ migration-library/src/main/java/com/exatask/platform/migration/MigrationApplication.java
@@ -1,6 +1,8 @@
 package com.exatask.platform.migration;
 
+import org.springframework.context.event.ContextRefreshedEvent;
+
 public interface MigrationApplication {
 
-  void migrateMongodb();
+  void migrateMongodb(ContextRefreshedEvent event);
 }
