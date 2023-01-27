c47adefb8dc35868fc0c7bd9858be94b9a0b8318
Rohit Aggarwal <er.rohitaggarwal1989@gmail.com>
2023-01-27 18:07:12 +0530

    Adding link generation in service, adding repo for beanstalk, cassandra, kafka and dynamodb

--- api-library/src/main/java/com/exatask/platform/api/services/AppService.java
+++ api-library/src/main/java/com/exatask/platform/api/services/AppService.java
@@ -1,13 +1,55 @@
 package com.exatask.platform.api.services;
 
+import com.exatask.platform.api.requests.ListRequest;
 import com.exatask.platform.logging.AppLogManager;
 import com.exatask.platform.logging.AppLogger;
 import com.exatask.platform.dto.requests.AppRequest;
 import com.exatask.platform.dto.responses.AppResponse;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class AppService<I extends AppRequest, O extends AppResponse> {
 
   protected static final AppLogger LOGGER = AppLogManager.getLogger();
 
   public abstract O process(I request);
+
+  protected List<Link> getLinks(ListRequest request, long count, Object invocationMethod) {
+
+    int page = request.getPage();
+    long totalPages = count / request.getLimit();
+    List<Link> links = new ArrayList<>();
+
+    if (page < totalPages) {
+
+      request.setPage(page + 1);
+      Link next = WebMvcLinkBuilder.linkTo(invocationMethod)
+          .withRel(IanaLinkRelations.NEXT);
+      links.add(next);
+
+      request.setPage((int) totalPages);
+      Link last = WebMvcLinkBuilder.linkTo(invocationMethod)
+          .withRel(IanaLinkRelations.LAST);
+      links.add(last);
+    }
+
+    if (page > 1) {
+
+      request.setPage(page - 1);
+      Link prev = WebMvcLinkBuilder.linkTo(invocationMethod)
+          .withRel(IanaLinkRelations.PREV);
+      links.add(prev);
+
+      request.setPage(1);
+      Link first = WebMvcLinkBuilder.linkTo(invocationMethod)
+          .withRel(IanaLinkRelations.FIRST);
+      links.add(first);
+    }
+
+    return links;
+  }
 }
--- mongodb-library/src/main/java/com/exatask/platform/mongodb/mappers/AppMongoMapper.java
+++ mongodb-library/src/main/java/com/exatask/platform/mongodb/mappers/AppMongoMapper.java
@@ -1,5 +1,6 @@
 package com.exatask.platform.mongodb.mappers;
 
+import org.apache.commons.lang3.ObjectUtils;
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Component;
 
@@ -7,6 +8,11 @@ import org.springframework.stereotype.Component;
 public class AppMongoMapper {
 
   public String objectIdToString(ObjectId objectId) {
+
+    if (ObjectUtils.isEmpty(objectId)) {
+      return null;
+    }
+
     return objectId.toHexString();
   }
 }
