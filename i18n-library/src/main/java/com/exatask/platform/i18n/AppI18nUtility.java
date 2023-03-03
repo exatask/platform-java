57553007abd6f6e8ea9f992e31abdd050e0f0759
Rohit Aggarwal <er.rohitaggarwal1989@gmail.com>
2023-03-03 12:53:08 +0530

    Improving initialization of translator, fixing crypto properties loading

--- i18n-library/src/main/java/com/exatask/platform/i18n/AppI18nUtility.java
+++ i18n-library/src/main/java/com/exatask/platform/i18n/AppI18nUtility.java
@@ -1,31 +1,10 @@
 package com.exatask.platform.i18n;
 
 import com.exatask.platform.i18n.constants.Locale;
-import com.exatask.platform.i18n.sources.AppI18nSource;
 import lombok.experimental.UtilityClass;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.util.CollectionUtils;
-
-import java.util.Set;
 
 @UtilityClass
 public class AppI18nUtility {
-
-  public static ResourceBundleMessageSource messageSource(Set<AppI18nSource> messageSources) {
-
-    ResourceBundleMessageSource bundleMessageSource = new ResourceBundleMessageSource();
-    bundleMessageSource.setDefaultEncoding(Locale.DEFAULT_ENCODING);
-    bundleMessageSource.setDefaultLocale(defaultLocale());
-    bundleMessageSource.setUseCodeAsDefaultMessage(true);
-
-    if (!CollectionUtils.isEmpty(messageSources)) {
-      for (AppI18nSource source : messageSources) {
-        bundleMessageSource.addBasenames(source.getMessageSources());
-      }
-    }
-
-    return bundleMessageSource;
-  }
 
   public static java.util.Locale defaultLocale() {
     return new java.util.Locale(Locale.DEFAULT_LANGUAGE, Locale.DEFAULT_COUNTRY_CODE);
