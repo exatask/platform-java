c9cfdf404a25b3825609af93084cdedf58472ed1
Rohit Aggarwal <er.rohitaggarwal1989@gmail.com>
2024-05-26 15:19:38 +0530

    Removing Aurora library, Mysql etc should be made compatible to it

--- subscriber-library/src/main/java/com/exatask/platform/subscriber/SubscriberRunner.java
+++ subscriber-library/src/main/java/com/exatask/platform/subscriber/SubscriberRunner.java
@@ -1,9 +1,11 @@
 package com.exatask.platform.subscriber;
 
+import com.exatask.platform.subscriber.commands.SubscriberCommand;
 import com.exatask.platform.subscriber.services.AppService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import picocli.CommandLine;
 
 import java.util.Set;
 
@@ -13,8 +15,16 @@ public class SubscriberRunner implements CommandLineRunner {
   @Autowired
   private Set<AppService> subscriberServices;
 
+  SubscriberCommand subscriberCommand = new SubscriberCommand();
+
   @Override
   public void run(String... args) {
+
+    CommandLine.ParseResult parsedCommand = subscriberCommand.getCommandLine().parseArgs(args);
+    boolean disableSubscriber = parsedCommand.hasMatchedOption("disable-subscriber");
+    if (disableSubscriber) {
+      return;
+    }
 
     for (AppService subscriberService : subscriberServices) {
       subscriberService.getCommandLine().execute(args);
