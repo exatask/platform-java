c58ea749764a823c3074c5bb4cce8c37cca59cc3
Rohit Aggarwal <er.rohitaggarwal1989@gmail.com>
2024-08-12 19:38:11 +0530

    Moving common code for reactive and non-reactive implementation to utility

--- mongodb-library/src/main/java/com/exatask/platform/mongodb/AppReactiveMongoRepositoryImpl.java
+++ mongodb-library/src/main/java/com/exatask/platform/mongodb/AppReactiveMongoRepositoryImpl.java
@@ -2,19 +2,12 @@ package com.exatask.platform.mongodb;
 
 import com.exatask.platform.logging.AppLogManager;
 import com.exatask.platform.logging.AppLogger;
-import com.exatask.platform.mongodb.constants.Defaults;
 import com.exatask.platform.mongodb.queries.AppQuery;
-import com.exatask.platform.mongodb.queries.filters.FilterElement;
-import com.exatask.platform.mongodb.queries.updates.UpdateElement;
-import com.exatask.platform.mongodb.system.exceptions.InvalidIdentifierException;
 import com.exatask.platform.mongodb.system.exceptions.InvalidOperationException;
-import com.exatask.platform.utilities.ResourceUtility;
+import com.exatask.platform.mongodb.utilities.RepositoryUtility;
 import com.mongodb.client.result.UpdateResult;
-import org.bson.types.ObjectId;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.FindAndModifyOptions;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
-import org.springframework.data.mongodb.core.query.Field;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.SerializationUtils;
 import org.springframework.data.mongodb.core.query.Update;
@@ -25,9 +18,6 @@ import reactor.core.publisher.Mono;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 public class AppReactiveMongoRepositoryImpl<T extends AppModel, ID extends Serializable> extends SimpleReactiveMongoRepository<T, ID> implements AppReactiveMongoRepository<T, ID> {
 
@@ -48,92 +38,15 @@ public class AppReactiveMongoRepositoryImpl<T extends AppModel, ID extends Seria
     this.mongoOperations = mongoOperations;
   }
 
-  /**
-   * @param query
-   * @param filters
-   */
-  private void prepareFilters(Query query, List<FilterElement> filters) {
-
-    if (filters == null) {
-      return;
-    }
-
-    for (FilterElement filterElement : filters) {
-      query.addCriteria(filterElement.getCriteria());
-    }
-  }
-
-  /**
-   * @param query
-   * @param projection
-   */
-  private void prepareProjection(Query query, Map<String, Boolean> projection) {
-
-    if (projection == null) {
-      return;
-    }
-
-    Field fields = query.fields();
-    for (Map.Entry<String, Boolean> field : projection.entrySet()) {
-      if (Boolean.TRUE.equals(field.getValue())) {
-        fields.include(field.getKey());
-      } else {
-        fields.exclude(field.getKey());
-      }
-    }
-  }
-
-  /**
-   * @param query
-   * @param sort
-   */
-  private void prepareSort(Query query, Map<String, Integer> sort) {
-
-    if (sort == null) {
-      return;
-    }
-
-    for (Map.Entry<String, Integer> entry : sort.entrySet()) {
-      Sort.Direction direction = entry.getValue() == 1 ? Sort.Direction.ASC : Sort.Direction.DESC;
-      query.with(Sort.by(direction, entry.getKey()));
-    }
-  }
-
-  /**
-   * @param update
-   * @param updates
-   */
-  private void prepareUpdates(Update update, List<UpdateElement> updates) {
-
-    if (updates == null) {
-      return;
-    }
-
-    for (UpdateElement updateElement : updates) {
-      updateElement.setUpdate(update);
-    }
-  }
-
   @Override
   public Flux<T> find(AppQuery query) {
 
-    Integer skip = Optional.ofNullable(query.getSkip()).orElse(Defaults.DEFAULT_SKIP);
-    if (skip < Defaults.MINIMUM_SKIP) {
-      skip = Defaults.MINIMUM_SKIP;
-    }
-
-    Integer limit = Optional.ofNullable(query.getLimit()).orElse(Defaults.DEFAULT_LIMIT);
-    if (limit < Defaults.MINIMUM_LIMIT) {
-      limit = Defaults.MINIMUM_LIMIT;
-    } else if (limit > Defaults.MAXIMUM_LIMIT) {
-      limit = Defaults.MAXIMUM_LIMIT;
-    }
-
     Query findQuery = new Query();
-    prepareFilters(findQuery, query.getFilters());
-    prepareProjection(findQuery, query.getProjections());
-    prepareSort(findQuery, query.getSorts());
-    findQuery.skip(skip).limit(limit);
+    RepositoryUtility.prepareFilters(findQuery, query.getFilters());
+    RepositoryUtility.prepareProjection(findQuery, query.getProjections());
+    RepositoryUtility.prepareSort(findQuery, query.getSorts());
+    findQuery.skip(RepositoryUtility.prepareSkip(query))
+        .limit(RepositoryUtility.prepareLimit(query));
 
     this.lastQuery = String.format("%s.find(%s).project(%s).sort(%s).skip(%d).limit(%d)",
         mongoEntityInformation.getCollectionName(),
@@ -150,11 +63,11 @@ public class AppReactiveMongoRepositoryImpl<T extends AppModel, ID extends Seria
   public Mono<T> findAndUpdate(AppQuery query) {
 
     Query findQuery = new Query();
-    prepareFilters(findQuery, query.getFilters());
-    prepareProjection(findQuery, query.getProjections());
+    RepositoryUtility.prepareFilters(findQuery, query.getFilters());
+    RepositoryUtility.prepareProjection(findQuery, query.getProjections());
 
     Update updateQuery = new Update();
-    prepareUpdates(updateQuery, query.getUpdates());
+    RepositoryUtility.prepareUpdates(updateQuery, query.getUpdates());
     updateQuery.set(UPDATED_AT, LocalDateTime.now());
 
     FindAndModifyOptions options = new FindAndModifyOptions();
@@ -176,9 +89,9 @@ public class AppReactiveMongoRepositoryImpl<T extends AppModel, ID extends Seria
   public Mono<T> findOne(AppQuery query) {
 
     Query findQuery = new Query();
-    prepareFilters(findQuery, query.getFilters());
-    prepareProjection(findQuery, query.getProjections());
-    prepareSort(findQuery, query.getSorts());
+    RepositoryUtility.prepareFilters(findQuery, query.getFilters());
+    RepositoryUtility.prepareProjection(findQuery, query.getProjections());
+    RepositoryUtility.prepareSort(findQuery, query.getSorts());
 
     this.lastQuery = String.format("%s.findOne(%s).project(%s).sort(%s)",
         mongoEntityInformation.getCollectionName(),
@@ -191,33 +104,10 @@ public class AppReactiveMongoRepositoryImpl<T extends AppModel, ID extends Seria
   }
 
   @Override
-  public Mono<T> findByIdentifier(String identifier) {
-
-    AppQuery.AppQueryBuilder queryBuilder = AppQuery.builder();
-    if (ObjectId.isValid(identifier)) {
-      queryBuilder.filter("_id", identifier);
-    } else if (ResourceUtility.isUrn(identifier)) {
-      queryBuilder.filter("urn", identifier);
-    } else {
-      throw new InvalidIdentifierException(identifier);
-    }
-
-    Query findQuery = new Query();
-    prepareFilters(findQuery, queryBuilder.build().getFilters());
-
-    this.lastQuery = String.format("%s.findOne(%s)",
-        mongoEntityInformation.getCollectionName(),
-        SerializationUtils.serializeToJsonSafely(findQuery.getQueryObject()));
-    LOGGER.trace(this.lastQuery);
-
-    return mongoOperations.findOne(findQuery, mongoEntityInformation.getJavaType(), mongoEntityInformation.getCollectionName());
-  }
-
-  @Override
   public Mono<Long> count(AppQuery query) {
 
     Query countQuery = new Query();
-    prepareFilters(countQuery, query.getFilters());
+    RepositoryUtility.prepareFilters(countQuery, query.getFilters());
 
     this.lastQuery = String.format("%s.count(%s)",
         mongoEntityInformation.getCollectionName(),
@@ -231,10 +121,10 @@ public class AppReactiveMongoRepositoryImpl<T extends AppModel, ID extends Seria
   public Mono<Boolean> updateOne(AppQuery query) {
 
     Query findQuery = new Query();
-    prepareFilters(findQuery, query.getFilters());
+    RepositoryUtility.prepareFilters(findQuery, query.getFilters());
 
     Update updateQuery = new Update();
-    prepareUpdates(updateQuery, query.getUpdates());
+    RepositoryUtility.prepareUpdates(updateQuery, query.getUpdates());
     updateQuery.set(UPDATED_AT, LocalDateTime.now());
 
     this.lastQuery = String.format("%s.updateOne(%s, %s)",
@@ -251,10 +141,10 @@ public class AppReactiveMongoRepositoryImpl<T extends AppModel, ID extends Seria
   public Mono<Boolean> updateAll(AppQuery query) {
 
     Query findQuery = new Query();
-    prepareFilters(findQuery, query.getFilters());
+    RepositoryUtility.prepareFilters(findQuery, query.getFilters());
 
     Update updateQuery = new Update();
-    prepareUpdates(updateQuery, query.getUpdates());
+    RepositoryUtility.prepareUpdates(updateQuery, query.getUpdates());
     updateQuery.set(UPDATED_AT, LocalDateTime.now());
 
     this.lastQuery = String.format("%s.updateMany(%s, %s)",
@@ -271,10 +161,10 @@ public class AppReactiveMongoRepositoryImpl<T extends AppModel, ID extends Seria
   public Mono<ID> upsert(AppQuery query) {
 
     Query findQuery = new Query();
-    prepareFilters(findQuery, query.getFilters());
+    RepositoryUtility.prepareFilters(findQuery, query.getFilters());
 
     Update updateQuery = new Update();
-    prepareUpdates(updateQuery, query.getUpdates());
+    RepositoryUtility.prepareUpdates(updateQuery, query.getUpdates());
     updateQuery.set(UPDATED_AT, LocalDateTime.now());
     updateQuery.setOnInsert(CREATED_AT, LocalDateTime.now());
 
